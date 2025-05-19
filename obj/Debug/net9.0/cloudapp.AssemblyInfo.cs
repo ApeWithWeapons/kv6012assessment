7991b74e286bba8ebdcb079b39398a6f042e74ea
ApeWithWeapons <almeslehbicher@gmail.com>
2025-05-19 12:45:53 +0100

    new branch for improvements

--- obj/Debug/net9.0/cloudapp.AssemblyInfo.cs
+++ obj/Debug/net9.0/cloudapp.AssemblyInfo.cs
@@ -10,10 +10,11 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("08402124-5bb6-4723-b08b-349dd883cf82")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("cloudapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5dde437666042ce1945b94fd252918ffdc8ba49c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6f009894efd062425e13ed13ee6909e5301c6d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("cloudapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cloudapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
