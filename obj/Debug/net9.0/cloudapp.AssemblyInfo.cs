bfde7090ecc3818ce60739f7b66876ed52816276
ApeWithWeapons <almeslehbicher@gmail.com>
2025-08-21 14:37:09 +0100

    deleting

--- obj/Debug/net9.0/cloudapp.AssemblyInfo.cs
+++ obj/Debug/net9.0/cloudapp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cloudapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+099ec9b0b51effee6c10d412f8aac922e7084349")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a178be2a69ce2aaf7899063778d37775cd239baa")]
 [assembly: System.Reflection.AssemblyProductAttribute("cloudapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cloudapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
